e92048c79484bdc1937b177714ae675ebab4e951
Meha Goyal <mehagoyal123@gmail.com>
2021-02-28 07:33:02 +0000

    Added CSS and Emergency Help Page

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
